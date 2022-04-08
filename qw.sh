5665ccedb161459e1a3423e36f9b5b87d70e23a9
“gincode18” <vishalkamboj9211@gmail.com>
2022-04-08 09:42:26 +0530

    exp

---
+++ qw.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+query=$(printf '%s' "$*" | tr ' ' '+' )
+o="+"
+n="%20"
+result=$(echo $query | sed "s/$o/$n/g")
+magnet=$(curl https://thepiratebay.party/search/$result/1/99/0 | grep -Po "magnet:\?xt=urn:btih:[a-zA-Z0-9]*" | head -n 1)
+
+curl https://thepiratebay.party/search/$result/1/99/0 | grep -Po "magnet:\?xt=urn:btih:[a-zA-Z0-9]*" | head -n 5 > txt
+echo "enter the magnet no from 1-5"
+read number
+if [ $number -eq 1 ]
+then
+    torrent=$(cat txt | sed -n ' 1 p')
+elif [ $number -eq 2 ] 
+then
+    torrent=$(cat txt | sed -n ' 2 p')
+elif [ $number -eq 3 ] 
+then
+    torrent=$(cat txt | sed -n ' 3 p')
+elif [ $number -eq 4 ] 
+then
+    torrent=$(cat txt | sed -n ' 4 p')
+elif [ $number -eq 5 ] 
+then
+    torrent=$(cat txt | sed -n ' 5 p')
+    
+fi
+webtorrent $torrent --mpv --playlist
+
+
